8c80b5d50fe227e9492a2a70e29e9e7c3fec454a
Prachi Sharma <145744960+prachisharma1277@users.noreply.github.com>
2026-01-31 16:34:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,15 @@ const app = express();
 app.use(express.json());
 app.use(cors()); 
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log('DB Connection Error:', err));
 
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/summarize', summarizeRoutes);
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
